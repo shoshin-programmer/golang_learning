7c8eec8aacf59e22946b03249558247dbb6cfb8e
Arvin Mark Atienza <61683592+markryu14@users.noreply.github.com>
2020-10-28 23:46:38 +0800

    hello world

---
+++ 1_hello_world.go
@@ -0,0 +1,13 @@
+package main
+// every go program should declare what the program is a part of 
+// main will be the entry point for go to start this as an executable
+
+import (
+	"fmt" // library to format strings
+)
+
+// main function will be the application entry point
+func main() {
+	fmt.Println("Hello, World!")
+	// simple print
+}
